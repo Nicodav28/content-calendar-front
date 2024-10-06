f964bf2310de913944883175488c17b65438cf11
Nicodav28 <esteveznicolas0@gmail.com>
2024-10-06 11:07:53 -0500

    📝 (calendar.model.ts): añadir interfaz Body a namespace NCalendar ✨ (calendar.component.html): actualizar la estructura HTML para mostrar los días del calendario con clases dinámicas basadas en las propiedades isCurrentMonth e isCurrentDay de cada elemento 🔧 (calendar.component.scss): añadir estilos CSS para resaltar los días actuales y del mes actual en el calendario ⬆️ (calendar.component.ts): refactorizar el componente de calendario para generar dinámicamente los elementos del calendario y resaltar el día actual Se han realizado cambios en el modelo de calendario para añadir una interfaz Body que define la estructura de los elementos del calendario. En el componente de calendario, se ha actualizado el HTML para mostrar los días del calendario con clases dinámicas que resaltan los días del mes actual y el día actual. Se han añadido estilos CSS para resaltar visualmente estos elementos. Además, se ha refactorizado el componente para generar dinámicamente los elementos del calendario y resaltar el día actual de forma dinámica.

--- src/app/model/calendar.model.ts
+++ src/app/model/calendar.model.ts
@@ -1,3 +1,9 @@
 export namespace NCalendar {
   export type Header = [string, string, string, string, string, string, string];
+
+  export interface Body {
+    day: number;
+    isCurrentDay: boolean;
+    isCurrentMonth: boolean;
+  }
 }
--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { NCalendar } from '../../model/calendar.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss',
 })
@@ -19,7 +20,85 @@ export class CalendarComponent {
     'Viernes',
     'Sabado',
   ];
-  public calendarElements: any[] = new Array(42).fill(1);
+
+  private readonly totalItems: number = 42;
+  private readonly currentDate: Date = new Date();
+  private readonly daysInMonthCache = new Map<string, number>();
+
+  public calendarElements: NCalendar.Body[] = [];
+
+  public constructor() {
+    this.createCalendarData();
+  }
+
+  private getDaysInMonth(year: number, month: number): number {
+    const key = `${year}-${month}`;
+    if (!this.daysInMonthCache.has(key)) {
+      const daysInMonth = new Date(year, month + 1, 0).getDate();
+      this.daysInMonthCache.set(key, daysInMonth);
+    }
+    return this.daysInMonthCache.get(key)!;
+  }
+
+  private createCalendarData(): void {
+    const year = this.currentDate.getFullYear();
+    const month = this.currentDate.getMonth();
+
+    const firstDayInMonth = this.getSelectedDate(year, month, 1).getDay();
+    const lastDayPreviousMonth = this.getDaysInMonth(year, month - 1);
+
+    this.calendarElements = Array.from(
+      { length: firstDayInMonth },
+      (_, index) => ({
+        day: lastDayPreviousMonth - index,
+        isCurrentDay: false,
+        isCurrentMonth: false,
+      })
+    );
+
+    const lastDayInMonth = this.getDaysInMonth(year, month);
+    const todayFormattedDate = this.formatDate(this.currentDate);
+    this.calendarElements.push(
+      ...Array.from({ length: lastDayInMonth }, (_, index) => {
+        const evaluatedFormattedDate = this.formatDate(
+          this.getSelectedDate(year, month, index + 1)
+        );
+
+        const isCurrentDay = this.isCurrentDay(
+          todayFormattedDate,
+          evaluatedFormattedDate
+        );
+
+        return {
+          day: index + 1,
+          isCurrentDay,
+          isCurrentMonth: true,
+        };
+      })
+    );
+
+    const totalDaysNeeded = this.totalItems - this.calendarElements.length;
+
+    this.calendarElements.push(
+      ...Array.from({ length: totalDaysNeeded }, (_, index) => ({
+        day: index + 1,
+        isCurrentDay: false,
+        isCurrentMonth: false,
+      }))
+    );
+  }
+
+  private formatDate(date: Date): string {
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  }
+
+  private getSelectedDate(year: number, month: number, day: number): Date {
+    return new Date(year, month, day);
+  }
+
+  private isCurrentDay(todayDate: string, comparisonDate: string): boolean {
+    return todayDate === comparisonDate;
+  }
 
   public openDialog() {
     console.log('asd');
